554903d8fa0bb67649ef7ccf17835ca7fc4f7169
PeterKron <89381357+PeterKron@users.noreply.github.com>
2022-02-03 16:36:09 +0100

    refactored to use VueX
    Co-authored-by: Editha Trinitaria <mammutakademin@users.noreply.github.com>

--- src/views/BookLibrary.vue
+++ src/views/BookLibrary.vue
@@ -1,33 +1,49 @@
 <template>
-<div>
-  <router-link to="/readinglist">
-    <button>Reading List</button>
-  </router-link>
-  <main>
+  <div>
+    <!-- <router-link to="/readinglist">
+      <button>Reading List</button>
+    </router-link> -->
+    <main>
     <router-view></router-view>
-    
-    <h1>8 Classic Children's books</h1>
-    <section class="gridlayout" >
-       <BookItem v-for="book in listOfBooks" :key="book.title" :book="book"/>
-    </section>
-  </main>
+    <!-- <router-link to="/"> -->
+    <!-- </router-link> -->
+      <h1>8 Classic Children's books</h1>
+      <section class="gridlayout" >
+        <button v-for="book in karlolof" :key="book.title" @click="addToReadingList(book)">{{book.author}}</button>
+      <!-- <p>{{singlebook}}</p> -->
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import books from '../assets/books.json'
-import BookItem from '../components/BookItem.vue'
+// import SingleBookViewVue from './SingleBookView.vue'
+// import books from '../assets/books.json'
+// import BookItem from '../components/BookItem.vue'
 export default {
-  props: ['library'],
-  components: {BookItem},
-    data(){return{
-    listOfBooks: books
-  }}
-
-
+  methods: {
+    addToReadingList(item){
+      // this.$store.commit('addToReadingList', item)
+      // this.$store.commit('singleBook', item)
+      this.$store.dispatch('bookRender', item)
+      this.$router.push('/singlebookview')
+    }
+  },
+  computed: {
+    karlolof(){
+      return this.$store.state.greger
+      },
+      singlebook(){
+      return this.$store.state.singlebook
+      }
+    },
+        // props: ['library'],
+        // components: {BookItem},
+        //   data(){return{
+        //   listOfBooks: books
+        // }},
 }
 </script>
-
 
 
 <style>
@@ -35,18 +51,14 @@ div{
   width: 100%;
 }
 main{
-
  display: flex;
  align-items: center;
  flex-direction: column;
- 
 }
 button{
   background-color: rgb(32, 32, 32);
   color:white;
 }
-
-
 .gridlayout{
   width: 35%;
   display:grid;
@@ -55,5 +67,4 @@ button{
   grid-gap: 20px;
   margin-top: 8px; 
 }
-
 </style>
